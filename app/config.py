5e3e4074c8387613861585a0eda8139d394a4225
Elefantat <135960191+Elefantat@users.noreply.github.com>
2025-08-24 17:01:30 +0800

    Update config.py

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ baseurl = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = 'mysecretkey'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(baseurl, 'app.db')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     AVATAR_FOLDER = os.path.join(os.path.dirname(__file__), 'static', 'image', 'avatars')
     UPLOAD_FOLDER = os.path.join(os.path.dirname(__file__), 'static', 'image', 'uploads')
